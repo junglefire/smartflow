37e914660bab27822123db6c65937734c6e0773b
王征 <alex.wz@alibaba-inc.com>
2020-12-04 14:55:29 +0800

    wip

--- smartflow/base/util.py
+++ smartflow/base/util.py
@@ -1,276 +1,265 @@
 # coding: utf-8
+from typing import Tuple
 import logging as log
 from .np import *
 import os
 
 # 
 # 生成文本的共现矩阵
-def preprocess(text):
-    text = text.lower()
-    text = text.replace('.', ' .')
-    words = text.split(' ')
-    word_to_id = {}
-    id_to_word = {}
-    for word in words:
-        if word not in word_to_id:
-            new_id = len(word_to_id)
-            word_to_id[word] = new_id
-            id_to_word[new_id] = word
-    corpus = np.array([word_to_id[w] for w in words])
-    return corpus, word_to_id, id_to_word
+def preprocess(text: str)-> Tuple[np.ndarray, dict, dict]:
+	text = text.lower()
+	text = text.replace('.', ' .')
+	words = text.split(' ')
+	word_to_id = {}
+	id_to_word = {}
+	for word in words:
+		if word not in word_to_id:
+			new_id = len(word_to_id)
+			word_to_id[word] = new_id
+			id_to_word[new_id] = word
+	corpus = np.array([word_to_id[w] for w in words])
+	return corpus, word_to_id, id_to_word
 
 
 #
 # 计算余弦相似度
 def cos_similarity(x, y, eps=1e-8):
-    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
-    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
-    return np.dot(nx, ny)
+	nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+	ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+	return np.dot(nx, ny)
 
 
 #
 # 查询相似度为TopN的单词
 def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
-    if query not in word_to_id:
-        log.info('%s is not found', query)
-        return
-    log.info('[query] %s', query)
-    query_id = word_to_id[query]
-    query_vec = word_matrix[query_id]
-    vocab_size = len(id_to_word)
-    similarity = np.zeros(vocab_size)
-    for i in range(vocab_size):
-        similarity[i] = cos_similarity(word_matrix[i], query_vec)
-    count = 0
-    for i in (-1 * similarity).argsort():
-        if id_to_word[i] == query:
-            continue
-        log.info(' - %s', (id_to_word[i], similarity[i]))
-        count += 1
-        if count >= top:
-            return
+	if query not in word_to_id:
+		log.info('%s is not found', query)
+		return
+	log.info('[query] %s', query)
+	query_id = word_to_id[query]
+	query_vec = word_matrix[query_id]
+	vocab_size = len(id_to_word)
+	similarity = np.zeros(vocab_size)
+	for i in range(vocab_size):
+		similarity[i] = cos_similarity(word_matrix[i], query_vec)
+	count = 0
+	for i in (-1 * similarity).argsort():
+		if id_to_word[i] == query:
+			continue
+		log.info(' - %s', (id_to_word[i], similarity[i]))
+		count += 1
+		if count >= top:
+			return
 
-
+#
+# one-hot编码
 def convert_one_hot(corpus, vocab_size):
-    '''one-hot表現への変換
+	N = corpus.shape[0]
+	if corpus.ndim == 1:
+		one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+		for idx, word_id in enumerate(corpus):
+			one_hot[idx, word_id] = 1
+	elif corpus.ndim == 2:
+		C = corpus.shape[1]
+		one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+		for idx_0, word_ids in enumerate(corpus):
+			for idx_1, word_id in enumerate(word_ids):
+				one_hot[idx_0, idx_1, word_id] = 1
+	return one_hot
 
-    :param corpus: 単語IDのリスト（1次元もしくは2次元のNumPy配列）
-    :param vocab_size: 語彙数
-    :return: one-hot表現（2次元もしくは3次元のNumPy配列）
-    '''
-    N = corpus.shape[0]
-
-    if corpus.ndim == 1:
-        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
-        for idx, word_id in enumerate(corpus):
-            one_hot[idx, word_id] = 1
-
-    elif corpus.ndim == 2:
-        C = corpus.shape[1]
-        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
-        for idx_0, word_ids in enumerate(corpus):
-            for idx_1, word_id in enumerate(word_ids):
-                one_hot[idx_0, idx_1, word_id] = 1
-
-    return one_hot
-
-
+#
+# 计算共现矩阵
+# 用元素为0的二维数组对co_matrix进行初始化。然后，针对语料库中的每一个单词，计算它的窗口中
+# 包含的单词。同时，检查窗口内的单词是否超出了语料库的左端和右端
 def create_co_matrix(corpus, vocab_size, window_size=1):
-    '''共起行列の作成
+	corpus_size = len(corpus)
+	co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
 
-    :param corpus: コーパス（単語IDのリスト）
-    :param vocab_size:語彙数
-    :param window_size:ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
-    :return: 共起行列
-    '''
-    corpus_size = len(corpus)
-    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+	for idx, word_id in enumerate(corpus):
+		for i in range(1, window_size + 1):
+			left_idx = idx - i
+			right_idx = idx + i
 
-    for idx, word_id in enumerate(corpus):
-        for i in range(1, window_size + 1):
-            left_idx = idx - i
-            right_idx = idx + i
+			if left_idx >= 0:
+				left_word_id = corpus[left_idx]
+				co_matrix[word_id, left_word_id] += 1
 
-            if left_idx >= 0:
-                left_word_id = corpus[left_idx]
-                co_matrix[word_id, left_word_id] += 1
+			if right_idx < corpus_size:
+				right_word_id = corpus[right_idx]
+				co_matrix[word_id, right_word_id] += 1
 
-            if right_idx < corpus_size:
-                right_word_id = corpus[right_idx]
-                co_matrix[word_id, right_word_id] += 1
-
-    return co_matrix
+	return co_matrix
 
 
 #
 # 正的点互信息
 def ppmi(C, verbose=False, eps=1e-8):
-    M = np.zeros_like(C, dtype=np.float32)
-    N = np.sum(C)
-    S = np.sum(C, axis=0)
-    total = C.shape[0] * C.shape[1]
-    cnt = 0
-    for i in range(C.shape[0]):
-        for j in range(C.shape[1]):
-            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
-            M[i, j] = max(0, pmi)
-            if verbose:
-                cnt += 1
-                if cnt % (total//100 + 1) == 0:
-                    log.info('%.1f%% done', (100*cnt/total))
-    return M
+	M = np.zeros_like(C, dtype=np.float32)
+	N = np.sum(C)
+	S = np.sum(C, axis=0)
+	total = C.shape[0] * C.shape[1]
+	cnt = 0
+	for i in range(C.shape[0]):
+		for j in range(C.shape[1]):
+			pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+			M[i, j] = max(0, pmi)
+			if verbose:
+				cnt += 1
+				if cnt % (total//100 + 1) == 0:
+					log.info('%.1f%% done', (100*cnt/total))
+	return M
 
 
 def create_contexts_target(corpus, window_size=1):
-    '''コンテキストとターゲットの作成
+	'''コンテキストとターゲットの作成
 
-    :param corpus: コーパス（単語IDのリスト）
-    :param window_size: ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
-    :return:
-    '''
-    target = corpus[window_size:-window_size]
-    contexts = []
+	:param corpus: コーパス（単語IDのリスト）
+	:param window_size: ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
+	:return:
+	'''
+	target = corpus[window_size:-window_size]
+	contexts = []
 
-    for idx in range(window_size, len(corpus)-window_size):
-        cs = []
-        for t in range(-window_size, window_size + 1):
-            if t == 0:
-                continue
-            cs.append(corpus[idx + t])
-        contexts.append(cs)
+	for idx in range(window_size, len(corpus)-window_size):
+		cs = []
+		for t in range(-window_size, window_size + 1):
+			if t == 0:
+				continue
+			cs.append(corpus[idx + t])
+		contexts.append(cs)
 
-    return np.array(contexts), np.array(target)
+	return np.array(contexts), np.array(target)
 
 
 def to_cpu(x):
-    import numpy
-    if type(x) == numpy.ndarray:
-        return x
-    return np.asnumpy(x)
+	import numpy
+	if type(x) == numpy.ndarray:
+		return x
+	return np.asnumpy(x)
 
 
 def to_gpu(x):
-    import cupy
-    if type(x) == cupy.ndarray:
-        return x
-    return cupy.asarray(x)
+	import cupy
+	if type(x) == cupy.ndarray:
+		return x
+	return cupy.asarray(x)
 
 
 def clip_grads(grads, max_norm):
-    total_norm = 0
-    for grad in grads:
-        total_norm += np.sum(grad ** 2)
-    total_norm = np.sqrt(total_norm)
+	total_norm = 0
+	for grad in grads:
+		total_norm += np.sum(grad ** 2)
+	total_norm = np.sqrt(total_norm)
 
-    rate = max_norm / (total_norm + 1e-6)
-    if rate < 1:
-        for grad in grads:
-            grad *= rate
+	rate = max_norm / (total_norm + 1e-6)
+	if rate < 1:
+		for grad in grads:
+			grad *= rate
 
 
 def eval_perplexity(model, corpus, batch_size=10, time_size=35):
-    print('evaluating perplexity ...')
-    corpus_size = len(corpus)
-    total_loss, loss_cnt = 0, 0
-    max_iters = (corpus_size - 1) // (batch_size * time_size)
-    jump = (corpus_size - 1) // batch_size
+	print('evaluating perplexity ...')
+	corpus_size = len(corpus)
+	total_loss, loss_cnt = 0, 0
+	max_iters = (corpus_size - 1) // (batch_size * time_size)
+	jump = (corpus_size - 1) // batch_size
 
-    for iters in range(max_iters):
-        xs = np.zeros((batch_size, time_size), dtype=np.int32)
-        ts = np.zeros((batch_size, time_size), dtype=np.int32)
-        time_offset = iters * time_size
-        offsets = [time_offset + (i * jump) for i in range(batch_size)]
-        for t in range(time_size):
-            for i, offset in enumerate(offsets):
-                xs[i, t] = corpus[(offset + t) % corpus_size]
-                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+	for iters in range(max_iters):
+		xs = np.zeros((batch_size, time_size), dtype=np.int32)
+		ts = np.zeros((batch_size, time_size), dtype=np.int32)
+		time_offset = iters * time_size
+		offsets = [time_offset + (i * jump) for i in range(batch_size)]
+		for t in range(time_size):
+			for i, offset in enumerate(offsets):
+				xs[i, t] = corpus[(offset + t) % corpus_size]
+				ts[i, t] = corpus[(offset + t + 1) % corpus_size]
 
-        try:
-            loss = model.forward(xs, ts, train_flg=False)
-        except TypeError:
-            loss = model.forward(xs, ts)
-        total_loss += loss
+		try:
+			loss = model.forward(xs, ts, train_flg=False)
+		except TypeError:
+			loss = model.forward(xs, ts)
+		total_loss += loss
 
-        sys.stdout.write('\r%d / %d' % (iters, max_iters))
-        sys.stdout.flush()
+		sys.stdout.write('\r%d / %d' % (iters, max_iters))
+		sys.stdout.flush()
 
-    print('')
-    ppl = np.exp(total_loss / max_iters)
-    return ppl
+	print('')
+	ppl = np.exp(total_loss / max_iters)
+	return ppl
 
 
 def eval_seq2seq(model, question, correct, id_to_char,
-                 verbos=False, is_reverse=False):
-    correct = correct.flatten()
-    # 頭の区切り文字
-    start_id = correct[0]
-    correct = correct[1:]
-    guess = model.generate(question, start_id, len(correct))
+				 verbos=False, is_reverse=False):
+	correct = correct.flatten()
+	# 頭の区切り文字
+	start_id = correct[0]
+	correct = correct[1:]
+	guess = model.generate(question, start_id, len(correct))
 
-    # 文字列へ変換
-    question = ''.join([id_to_char[int(c)] for c in question.flatten()])
-    correct = ''.join([id_to_char[int(c)] for c in correct])
-    guess = ''.join([id_to_char[int(c)] for c in guess])
+	# 文字列へ変換
+	question = ''.join([id_to_char[int(c)] for c in question.flatten()])
+	correct = ''.join([id_to_char[int(c)] for c in correct])
+	guess = ''.join([id_to_char[int(c)] for c in guess])
 
-    if verbos:
-        if is_reverse:
-            question = question[::-1]
+	if verbos:
+		if is_reverse:
+			question = question[::-1]
 
-        colors = {'ok': '\033[92m', 'fail': '\033[91m', 'close': '\033[0m'}
-        print('Q', question)
-        print('T', correct)
+		colors = {'ok': '\033[92m', 'fail': '\033[91m', 'close': '\033[0m'}
+		print('Q', question)
+		print('T', correct)
 
-        is_windows = os.name == 'nt'
+		is_windows = os.name == 'nt'
 
-        if correct == guess:
-            mark = colors['ok'] + '☑' + colors['close']
-            if is_windows:
-                mark = 'O'
-            print(mark + ' ' + guess)
-        else:
-            mark = colors['fail'] + '☒' + colors['close']
-            if is_windows:
-                mark = 'X'
-            print(mark + ' ' + guess)
-        print('---')
+		if correct == guess:
+			mark = colors['ok'] + '☑' + colors['close']
+			if is_windows:
+				mark = 'O'
+			print(mark + ' ' + guess)
+		else:
+			mark = colors['fail'] + '☒' + colors['close']
+			if is_windows:
+				mark = 'X'
+			print(mark + ' ' + guess)
+		print('---')
 
-    return 1 if guess == correct else 0
+	return 1 if guess == correct else 0
 
 
 def analogy(a, b, c, word_to_id, id_to_word, word_matrix, top=5, answer=None):
-    for word in (a, b, c):
-        if word not in word_to_id:
-            print('%s is not found' % word)
-            return
+	for word in (a, b, c):
+		if word not in word_to_id:
+			print('%s is not found' % word)
+			return
 
-    print('\n[analogy] ' + a + ':' + b + ' = ' + c + ':?')
-    a_vec, b_vec, c_vec = word_matrix[word_to_id[a]], word_matrix[word_to_id[b]], word_matrix[word_to_id[c]]
-    query_vec = b_vec - a_vec + c_vec
-    query_vec = normalize(query_vec)
+	print('\n[analogy] ' + a + ':' + b + ' = ' + c + ':?')
+	a_vec, b_vec, c_vec = word_matrix[word_to_id[a]], word_matrix[word_to_id[b]], word_matrix[word_to_id[c]]
+	query_vec = b_vec - a_vec + c_vec
+	query_vec = normalize(query_vec)
 
-    similarity = np.dot(word_matrix, query_vec)
+	similarity = np.dot(word_matrix, query_vec)
 
-    if answer is not None:
-        print("==>" + answer + ":" + str(np.dot(word_matrix[word_to_id[answer]], query_vec)))
+	if answer is not None:
+		print("==>" + answer + ":" + str(np.dot(word_matrix[word_to_id[answer]], query_vec)))
 
-    count = 0
-    for i in (-1 * similarity).argsort():
-        if np.isnan(similarity[i]):
-            continue
-        if id_to_word[i] in (a, b, c):
-            continue
-        print(' {0}: {1}'.format(id_to_word[i], similarity[i]))
+	count = 0
+	for i in (-1 * similarity).argsort():
+		if np.isnan(similarity[i]):
+			continue
+		if id_to_word[i] in (a, b, c):
+			continue
+		print(' {0}: {1}'.format(id_to_word[i], similarity[i]))
 
-        count += 1
-        if count >= top:
-            return
+		count += 1
+		if count >= top:
+			return
 
 
 def normalize(x):
-    if x.ndim == 2:
-        s = np.sqrt((x * x).sum(1))
-        x /= s.reshape((s.shape[0], 1))
-    elif x.ndim == 1:
-        s = np.sqrt((x * x).sum())
-        x /= s
-    return x
+	if x.ndim == 2:
+		s = np.sqrt((x * x).sum(1))
+		x /= s.reshape((s.shape[0], 1))
+	elif x.ndim == 1:
+		s = np.sqrt((x * x).sum())
+		x /= s
+	return x
